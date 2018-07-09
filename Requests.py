b217a2c76fbae718989e8786c9f761dbb6a35f66
romanticyoung <strive0903@gmail.com>
2018-07-09 22:28:03 +0800

    headers

--- Requests.py
+++ Requests.py
@@ -3,14 +3,22 @@
 
 # 导入requests包
 import requests
+
 # get请求方法
-r = requests.get("http://www.baidu.com/")
+#r = requests.get("http://www.baidu.com/")
+# 各种请求方式
+#r = requests.post("http://httpbin.org/post")
+
 # requests 接受三个参数
 #r = requests.get(url, params=None, **kwargs)
 # 其中**kwargs包括如下：
 # headers:字典，HTTP定制头
 
-
+# 添加headers 谷歌浏览器chrome://version
+headers = {
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+}
+r = requests.get("https://www.zhihu.com",headers=headers)
 
 
 # 传递URL参数
@@ -19,6 +27,20 @@ r = requests.get("http://www.baidu.com/")
 #r = requests.get(url ,params=params)
 #print(r.url)
 
-# 编码
+
+# json
+#print(r.json)
+# 编码 utf-8 ISO-8859-1
 print(r.encoding)
-print(r.text)
+# 类型
+print(type(r))
+# 输出文本内容
+#print(r.text)
+# 输出状态码 200表示成功
+print(r.status_code)
+# 输出二进制
+#print(r.content)
+# 转换为utf-8
+#print(r.content.decode('utf-8'))
+
+
