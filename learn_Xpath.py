f811d91f68e6dc8a998eb1e1ec9b3efc0a956efe
romanticyoung <strive0903@gmail.com>
2018-07-11 15:46:26 +0800

    xpath latest

--- learn_Xpath.py
+++ learn_Xpath.py
@@ -3,6 +3,9 @@
 '''
 爬虫入门到精通-网页的解析（xpath）https://zhuanlan.zhihu.com/p/25572729
 requests获取数据，xpath解析数据，pandas保存数据
+xpath是一门在XML文档中查找信息的语言，可用来在XML文档中对元素和属性进行遍历。
+path输入html，输出xml
+path有七种类型节点：元素、属性、文本、命令空间、处理指令、注释及文档节点（根节点）
 '''
 
 import requests
@@ -51,6 +54,7 @@ print(s.xpath('//text()'))
 print(s.xpath('//comment()'))
 '''
 
+'''
 sample2 = """
 <html>
   <body>
@@ -79,5 +83,42 @@ print(s2.xpath('//li[a]/text()')) # ['Quote 2 with ', 'Quote 3 with ']
 print(s2.xpath('//li[a or h2]/text()')) # ['Quote 2 with ', 'Quote 3 with ', ' ...']
 # 输出a和h2
 print(s2.xpath('//a/text()|//h2/text()')) # ['link', 'another link', 'Quote 4 title']
+
+'''
+
+sample4 = u"""
+<html>
+  <head>
+    <title>My page</title>
+  </head>
+  <body>
+    <h2>Welcome to my <a href="#" src="x">page</a></h2>
+    <p>This is the first paragraph.</p>
+    <p class="test">
+    编程语言<a href="#">python</a>
+    <img src="#" alt="test"/>javascript
+    <a href="#"><strong>C#</strong>JAVA</a>
+    </p>
+    <p class="content-a">a</p>
+    <p class="content-b">b</p>
+    <p class="content-c">c</p>
+    <p class="content-d">d</p>
+    <p class="econtent-e">e</p>
+    <p class="heh">f</p>
+    <!-- this is the end -->
+  </body>
+</html>
+"""
+s4 = etree.HTML(sample4) # HTML转为XML
+
+# 获取p标签下的所有class中的test
+print(s4.xpath('//p[@class="test"]/text()')) #['\n    编程语言', '\n    ', 'javascript\n    ', '\n    ']
+# 获取p下所有文字 使用string获取某个标签下所有的文本
+print(s4.xpath('string(//p[@class="test"])'))
+# 获取p下所有class中的conten内容 starts-with 匹配字符串前面相等
+print(s4.xpath('//p[starts-with(@class,"content")]/text()')) # ['a', 'b', 'c', 'd']
+# contains 匹配任何位置相等
+print(s4.xpath('//p[contains(@class,"content")]/text()')) #['a', 'b', 'c', 'd', 'e']
+
 
 
